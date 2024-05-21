8b063e5561802470cc6ff521b2d7b08c4ba5d5a0
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-05-21 08:48:18 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version  
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+
+let name = "tejas" // string
+let age = 22 // num
+let islogin = false //boolean
+
+// Data types
+
+//number => 2 to power 53
+//bigint  => for storing big number
+//boolean => true or false
+// null => stand alone value(empty)
+// undefined => value not assign
+// symbol => find uniqueness
+
+
+// Object
+
+console.log(typeof age);
+name = "hitesh"
+console.log(name);
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,11 @@
 console.log("Welcome to VS studio");
+
+
+//const :- if you use const you cannot change the value of variable
+
+//let and var can be change
+
+/*
+Prefer not to use var
+because of issue in block  scope and functional scope
+*/
