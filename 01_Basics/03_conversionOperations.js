9f7e5b2fd1ebd5154b61f0b3c15ae307aa46275d
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-05-21 13:35:04 +0000

    Datatyp js

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,46 @@
+let score = "33e"
+//console.log(typeof score);
+let valueInNumber = Number(score);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false => 0
+
+
+let isLogin = 1
+
+let booleanisLogin = Boolean(isLogin)
+
+//  console.log(booleanisLogin);
+
+// 1 => TRUE
+// 0 OR EMPTY = false
+
+let stringNum = 23
+
+let strvalue = String(stringNum)
+//console.log(strvalue);
+//console.log(typeof strvalue);
+
+// covert to number to string
+
+
+//**************Operations*********************
+// condfusing conversions
+// mostly avoid these comparisons 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// ===
+console.log(null > 0);
+console.log(0 === 0);
+
---
+++ 01_Basics/04_datatypeSum.js
@@ -0,0 +1,35 @@
+// Primitive Data types
+
+// 7 types data : number, string, boolean, null, undefined, symbol, bigint;
+// for example take the Referenc from #conversionOperations file
+
+// Non primitive(Reference)
+
+// 3 types : Array, Object, Functions
+
+// Arrray
+
+const heros = ["Hulk", "Batman", "Ironman"]
+
+//object
+let obj = {
+    name : "Tejas",
+    age : 22,
+    isCitizen: true,
+};
+//console.log(obj);
+
+//  Functions
+function myfun(){
+    console.log("Hii Hitesh, nice to meet you");
+}
+// console.log(myfun);
+myfun();
+
+
+
+//******************Stack And Heap memory 
+
+//stack (Primitive)
+
+//Heap(Non primitive)
