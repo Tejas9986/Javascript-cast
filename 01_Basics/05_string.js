57a199c787fcbc10d32364687b62cef2fbda477d
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-05-22 06:13:04 +0000

    string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,20 @@
+const name = "tejas"
+const repoCount = 50
+
+//console.log(name+ repoCount + "tejas");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("hitesh-hc-com")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+
+const url = "https://urban-halibut-59667p6777jf75qw.github.dev/"
+
+console.log(url.replace('urban', 'modern')) // its replace the keyword.
+console.log(url.includes('github'))    // to check the keyword is present or not
+
+console.log(gameName.split('-'));   // to split the sentence or word from the keyword or string
