aaba364d2e9de102ec855347d2650167e552f5bc
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-05-23 06:20:12 +0000

    num

---
+++ 01_Basics/06_numMath.js
@@ -0,0 +1,23 @@
+// console.log("Bhenchod, Aaighal aata nit");
+
+const score = 400
+
+// console.log(score);
+
+// const balance = new Number(100.092)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+
+// **********MATHS***************
+
+console.log(Math);
+
+console.log(Math.abs(-23));
+
+console.log(Math.max(21));
+console.log(Math.min(21, 12));
+
+console.log(Math.random());
+// in math.random() value is between 0 to 1 only
