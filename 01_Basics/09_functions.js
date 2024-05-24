921295b97d85248f11e5230d77a9d3a8229fef2a
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-05-24 14:07:51 +0000

    functions

--- 01_Basics/09_functions.js
+++ 01_Basics/09_functions.js
@@ -5,7 +5,29 @@ function myfun(){
 // myfun()
 for(let i =0; i <5; i++){
     //console.log(myfun());
-    myfun()
+  //  myfun()
 
 }
 
+// function sum(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// sum(4, 7)
+
+
+function sum(num1, num2){
+    // let result = num1+num2    // its has to declare in function then it will print out otherwise it gives "undefined"
+    // return result
+
+    return num1 * num2
+}
+const result = sum(6, 8)
+// console.log("Result :", result);
+
+
+function User(username){
+    return `${username} user logged in`
+}
+
+console.log(User("Tejas Waditke"));
