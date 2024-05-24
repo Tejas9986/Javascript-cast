0d964232dcf72a0b2050c8d704593d414fd9a8fd
Tejas Waditke <83941514+Tejas9986@users.noreply.github.com>
2024-05-24 07:19:10 +0000

    object

--- 01_Basics/08_objects.js
+++ 01_Basics/08_objects.js
@@ -23,19 +23,44 @@ JsUser.email = "tejaswaditke35@gmail.com"
 // Object.freeze(JsUser);
 
 JsUser.email = "tejaswaditke72@gmail.com"
-console.log(JsUser.email);
+//console.log(JsUser.email);
 
 JsUser.greeting = function(){
-    console.log(`Hello JsUser `);
+    //console.log(`Hello JsUser `);
 }
 
 JsUser.greetingTwo = function(){
-    console.log(`Hello JsUser ${this.lastLoginDays}`);  // 'this' is used to use key of OBJECT directly
+    //console.log(`Hello JsUser ${this.lastLoginDays}`);  // 'this' is used to use key of OBJECT directly
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
 
 
 // use case use the dot to declare the object values or key
 // use [] for 'symbol' to declare inside the object
+
+
+// Non singleton Objects
+
+// const tinderUser = new Object() // its singleton object
+
+const tinderUser = {}   // its non singleton object
+
+tinderUser.Id = "Tej22"
+tinderUser.name = "Hitesh"
+tinderUser.isLoggedin = true
+//console.log(tinderUser.isLoggedin);
+
+const tcs = {
+    employeeId : 1267,
+    fullName:{
+        firstName : "Sam",
+        middleName : "Wilson",
+        lastName : "Potter"
+    },
+    domain : "techie support",
+    isLoggedoff : false,
+}
+
+console.log(tcs.fullName.middleName);
